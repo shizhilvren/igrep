d5ff8e17354e77d70c2e715638010f85efb2c64c
shizhilvren <shizhilvren@foxmail.com>
2025-07-04 21:14:51 +0800

    wasm 初始化

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,20 +1,13 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-import Search from './components/Search.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Search from './components/Search.vue';
+
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header> -->
-
   <main>
-    <Search msg_pass="this is pass msg"/>
+    <Search msg="this is a msg" />
   </main>
 </template>
 
