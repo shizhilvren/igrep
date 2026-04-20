8d94a3529d772a9411640b1a77bc6e83fd096036
shizhilvren <shizhilvren@foxmail.com>
2026-04-20 03:14:42 -0700

    v86 quick start with clangd

--- v86-image-build/alpine/build-state.js
+++ v86-image-build/alpine/build-state.js
@@ -19,21 +19,28 @@ var emulator = new V86({
     bios: { url: path.join(V86_ROOT, "bios/seabios.bin") },
     vga_bios: { url: path.join(V86_ROOT, "bios/vgabios.bin") },
     autostart: true,
-    memory_size: 512 * 1024 * 1024,
+    memory_size: 1 * 1024 * 1024 * 1024,
     vga_memory_size: 8 * 1024 * 1024,
     network_relay_url: "<UNUSED>",
     bzimage_initrd_from_filesystem: true,
     cmdline: "rw root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose modules=virtio_pci tsc=reliable init_on_free=on",
+    disable_keyboard: false,
+    disable_mouse: false,
+    serial_console: { type: "none" },
+    virtio_console: { type: "none" },
     filesystem: {
         baseurl: path.join(IMAGE_ROOT, "images/alpine-rootfs-flat"),
         basefs: path.join(IMAGE_ROOT, "images/alpine-fs.json"),
     },
+
 });
 
 console.log("Now booting, please stand by ...");
 
 let serial_text = "";
 let booted = false;
+let num = 0;
+
 
 emulator.add_listener("serial0-output-byte", function (byte) {
     const c = String.fromCharCode(byte);
@@ -42,25 +49,27 @@ emulator.add_listener("serial0-output-byte", function (byte) {
     serial_text += c;
     console.log(serial_text)
 
-    let num = 0;
-    if (!booted && serial_text.endsWith("(none):~# ")) {
+    if (!booted && (serial_text.endsWith("(none):~# ") || serial_text.endsWith("stdin/stdout"))) {
         if (num == 0) {
+            num = num + 1;
             emulator.serial0_send("which clangd\n");
+        } else if (num == 1) {
+            num = num + 1;
             emulator.serial0_send("clangd\n");
-            num++;
+        } else {
+            booted = true;
+            console.log(serial_text)
+            // emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
+
+            setTimeout(async function () {
+                const s = await emulator.save_state();
+
+                fs.writeFile(OUTPUT_FILE, new Uint8Array(s), function (e) {
+                    if (e) throw e;
+                    console.log("Saved as " + OUTPUT_FILE);
+                    emulator.destroy();
+                });
+            }, 10 * 1000);
         }
-        booted = true;
-        console.log(serial_text)
-        emulator.serial0_send("sync;echo 3 >/proc/sys/vm/drop_caches\n");
-
-        setTimeout(async function () {
-            const s = await emulator.save_state();
-
-            fs.writeFile(OUTPUT_FILE, new Uint8Array(s), function (e) {
-                if (e) throw e;
-                console.log("Saved as " + OUTPUT_FILE);
-                emulator.destroy();
-            });
-        }, 10 * 1000);
     }
 });
--- web-src/views/VMView.vue
+++ web-src/views/VMView.vue
@@ -34,11 +34,15 @@ import BIOS_URL from "../../v86/bios/seabios.bin?url";
 import VGA_BIOS_URL from "../../v86/bios/vgabios.bin?url";
 import FREEDOS_URL from "../../VM/freedos722.img?url";
 import ALPINE_URL from "../../VM/alpine_v86_edge_x86.bin?url";
+import FILE_SYSTEM_URL from "../../v86-image-build/alpine/images/alpine-fs.json?url";
+import INIT_STATE from "../../v86-image-build/alpine/images/alpine-state.bin?url";
 
+const FILE_SYSTEM_FILE = import.meta.env.BASE_URL + "../../v86-image-build/alpine/images/alpine-rootfs-flat";
 const BIOS_IMAGE = { url: BIOS_URL } as unknown as V86Image;
 const VGA_BIOS_IMAGE = { url: VGA_BIOS_URL } as unknown as V86Image;
 const FREEDOS_IMAGE = { url: FREEDOS_URL } as unknown as V86Image;
 const ALPINE_IMAGE = { url: ALPINE_URL, async: false } as unknown as V86Image;
+const INIT_STATE_IMAGE = { url: INIT_STATE, async: false } as unknown as V86Image;
 
 const status = ref("Loading...");
 const running = ref(false);
@@ -102,20 +106,27 @@ async function startVm(): Promise<void> {
         serialInput.value = "";
         emulator = new V86({
             wasm_path: WASM_PATH,
-            memory_size: 1 * 1024 * 1024 * 1024,
-            vga_memory_size: 8 * 1024 * 1024,
             bios: BIOS_IMAGE,
             vga_bios: VGA_BIOS_IMAGE,
-            // fda: FREEDOS_IMAGE,
-            cdrom: ALPINE_IMAGE,
             autostart: true,
+            memory_size: 1 * 1024 * 1024 * 1024,
+            vga_memory_size: 8 * 1024 * 1024,
+            network_relay_url: "<UNUSED>",
+            bzimage_initrd_from_filesystem: true,
+            cmdline: "rw root=host9p rootfstype=9p rootflags=trans=virtio,cache=loose modules=virtio_pci tsc=reliable init_on_free=on",
+            // fda: FREEDOS_IMAGE,
+            // cdrom: ALPINE_IMAGE,
             disable_keyboard: false,
             disable_mouse: false,
             serial_console: { type: "none" },
             virtio_console: { type: "none" },
             screen_container: target,
-            filesystem: {},
-            cmdline: "tsc=reliable mitigations=off random.trust_cpu=on",
+            // cmdline: "tsc=reliable mitigations=off random.trust_cpu=on",
+            filesystem: {
+                baseurl: FILE_SYSTEM_FILE,
+                basefs: FILE_SYSTEM_URL,
+            },
+            initial_state: INIT_STATE_IMAGE,
         });
 
         if (disposed && emulator) {
